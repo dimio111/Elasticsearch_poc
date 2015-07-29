026ac8ce088644d0b81e9402e9c6095475fc9b36
Dimitri Saeys <dimi.s89@hotmail.com>
2015-07-29 20:38:06 +0100

    Initial commit

---
+++ src/main/java/dimi/elasticsearch/poc/Main.java
@@ -0,0 +1,11 @@
+package dimi.elasticsearch.poc;
+
+/**
+ * Created by dimitrisaeys on 29/07/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/dimi/elasticsearch/poc/model/Product.java
@@ -0,0 +1,26 @@
+package dimi.elasticsearch.poc.model;
+
+/**
+ * Created by dimitrisaeys on 29/07/15.
+ */
+public class Product {
+
+    String title;
+    String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
