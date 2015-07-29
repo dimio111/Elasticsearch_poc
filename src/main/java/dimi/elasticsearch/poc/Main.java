07a7d44032649950fb5e9be90c10fe92f94b5d19
Dimitri Saeys <dimi.s89@hotmail.com>
2015-07-29 20:50:42 +0100

    Added stuff

--- src/main/java/dimi/elasticsearch/poc/Main.java
+++ src/main/java/dimi/elasticsearch/poc/Main.java
@@ -1,5 +1,10 @@
 package dimi.elasticsearch.poc;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
 /**
  * Created by dimitrisaeys on 29/07/15.
  */
@@ -7,5 +12,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Node node = nodeBuilder().clusterName("dimi_elasticsearch").node();
+        Client client = node.client();
+
+
+        node.close();
     }
 }
